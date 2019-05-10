f36a6c191380a6b4f01279eb6a1cd2144f0c280a
echao2012 <47439716+echao2012@users.noreply.github.com>
2019-05-10 13:30:34 -0500

    Connected form input data with state variables

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -2,199 +2,166 @@ import React, { Component } from "react";
 import { MDBSelect, MDBSelectInput, MDBSelectOptions, MDBSelectOption } from "mdbreact";
 import "./style.css";
 
-// function Form(props) {
-class Form extends Component {
-  state = {
-    options: [
-      {
-        checked: false,
-        disabled: false,
-        icon: null,
-        text: "Option One",
-        value: "1"
-      },
-      {
-        checked: false,
-        disabled: false,
-        icon: null,
-        text: "Option two",
-        value: "2"
-      },
-      {
-        checked: false,
-        disabled: false,
-        icon: null,
-        text: "Option three",
-        value: "3"
-      },
-      {
-        checked: false,
-        disabled: false,
-        icon: null,
-        text: "Option four",
-        value: "4"
-      }
-    ]
-  };
-
-  // build function for your selects, and pass it as getValue property to reed the select's value
-  getValueOfSelect = value => {
-    console.log(value);
-  };
-
-  remove = () => {
-    this.setState({
-      options: this.state.options.slice(0, -1)
-    });
-  };
-
-  render() {
-    return (
-      <div className="container">
-        <form>
-          <div className="form">
-            <input type="text" className="form-control-lg" id="search-box" aria-describedby="emailHelp" placeholder="Enter City to Search" />
-          </div>
-          <div className="card card-body">
-
-            
-
-            <div className="row upper">
-              <div className="col-md-6">
+function Form(props) {
+  return (
+    <div className="container">
+      <form>
+        <div className="form">
+          <input
+            type="text"
+            className="form-control-lg"
+            id="search-box"
+            placeholder="Enter City to Search"
+            name="location"
+            value={props.locationValue}
+            onChange={props.onChange}
+          />
+        </div>
+        <div className="card card-body">
+          <div className="row upper">
+            <div className="col-md-6">
               <h5>Primary Skill</h5>
-                <MDBSelect
-                  getValue={this.getValueOfSelect}
-                  getTextContent={this.getValueOfSelect}
-                // multiple
-
-                // uncomment the above line / one word of "multiple" to make this a multiple selection dropdown
-                >
-                  <MDBSelectInput selected="Choose one" />
-                  <MDBSelectOptions search>
-                    <MDBSelectOption disabled>Choose one</MDBSelectOption>
-                    <MDBSelectOption value="Angular">Angular</MDBSelectOption>
-                    <MDBSelectOption value="ASP">ASP/ASP.NET</MDBSelectOption>
-                    <MDBSelectOption value="AWS">AWS</MDBSelectOption>
-                    <MDBSelectOption value="C#">C#</MDBSelectOption>
-                    <MDBSelectOption value="C++">C++</MDBSelectOption>
-                    <MDBSelectOption value="CSS">CSS</MDBSelectOption>
-                    <MDBSelectOption value="Debian">Debian</MDBSelectOption>
-                    <MDBSelectOption value="Drupal">Drupal</MDBSelectOption>
-                    <MDBSelectOption value="Elixir">Elixir/Erlang</MDBSelectOption>
-                    <MDBSelectOption value="Golang">Golang</MDBSelectOption>
-                    <MDBSelectOption value="GCP">Google GCP</MDBSelectOption>
-                    <MDBSelectOption value="Haskell">Haskell</MDBSelectOption>
-                    <MDBSelectOption value="Java">Java</MDBSelectOption>
-                    <MDBSelectOption value="JavaScript">JavaScript</MDBSelectOption>
-                    <MDBSelectOption value="Laravel">Laravel</MDBSelectOption>
-                    <MDBSelectOption value="Linux">Linux</MDBSelectOption>
-                    <MDBSelectOption value="Lisp">Lisp</MDBSelectOption>
-                    <MDBSelectOption value="MySQL">MySQL</MDBSelectOption>
-                    <MDBSelectOption value="NodeJS">NodeJS</MDBSelectOption>
-                    <MDBSelectOption value="Objective-C">Objective-C</MDBSelectOption>
-                    <MDBSelectOption value="OCaml">OCaml</MDBSelectOption>
-                    <MDBSelectOption value="Pascal">Pascal</MDBSelectOption>
-                    <MDBSelectOption value="Perl">Perl</MDBSelectOption>
-                    <MDBSelectOption value="PostgreSQL">PostgreSQL</MDBSelectOption>
-                    <MDBSelectOption value="PostScript">PostScript</MDBSelectOption>
-                    <MDBSelectOption value="PHP">PHP</MDBSelectOption>
-                    <MDBSelectOption value="Python">Python</MDBSelectOption>
-                    <MDBSelectOption value="React">React</MDBSelectOption>
-                    <MDBSelectOption value="Ruby">Ruby on Rails</MDBSelectOption>
-                    <MDBSelectOption value="Rust">Rust</MDBSelectOption>
-                    <MDBSelectOption value="Scala">Scala</MDBSelectOption>
-                    <MDBSelectOption value="SQL">SQL</MDBSelectOption>
-                    <MDBSelectOption value="Swift">Swift</MDBSelectOption>
-                    <MDBSelectOption value="Ubuntu">Ubuntu</MDBSelectOption>
-                    <MDBSelectOption value="Unix">Unix</MDBSelectOption>
-                    <MDBSelectOption value="Vue">Vue</MDBSelectOption>
-                    <MDBSelectOption value="WordPress">WordPress</MDBSelectOption>
-                  </MDBSelectOptions>
-                </MDBSelect>
-              {/* <label>Primary Skill:</label> */}
-              </div>
-              <div className="col-md-6">
-              <h5>Additional Skills</h5>
-                <MDBSelect
-                  getValue={this.getValueOfSelect}
-                  getTextContent={this.getValueOfSelect}
-                  selected="Choose multiple"
-                  multiple
-                >
-                  <MDBSelectInput selected="Choose multiple" />
-                  <MDBSelectOptions search>
-                    <MDBSelectOption disabled>Choose multiple</MDBSelectOption>
-                    <MDBSelectOption value="Angular">Angular</MDBSelectOption>
-                    <MDBSelectOption value="ASP">ASP/ASP.NET</MDBSelectOption>
-                    <MDBSelectOption value="AWS">AWS</MDBSelectOption>
-                    <MDBSelectOption value="C#">C#</MDBSelectOption>
-                    <MDBSelectOption value="C++">C++</MDBSelectOption>
-                    <MDBSelectOption value="CSS">CSS</MDBSelectOption>
-                    <MDBSelectOption value="Debian">Debian</MDBSelectOption>
-                    <MDBSelectOption value="Drupal">Drupal</MDBSelectOption>
-                    <MDBSelectOption value="Elixir">Elixir/Erlang</MDBSelectOption>
-                    <MDBSelectOption value="Golang">Golang</MDBSelectOption>
-                    <MDBSelectOption value="GCP">Google GCP</MDBSelectOption>
-                    <MDBSelectOption value="Haskell">Haskell</MDBSelectOption>
-                    <MDBSelectOption value="HTML">HTML</MDBSelectOption>
-                    <MDBSelectOption value="Java">Java</MDBSelectOption>
-                    <MDBSelectOption value="JavaScript">JavaScript</MDBSelectOption>
-                    <MDBSelectOption value="Laravel">Laravel</MDBSelectOption>
-                    <MDBSelectOption value="Linux">Linux</MDBSelectOption>
-                    <MDBSelectOption value="Lisp">Lisp</MDBSelectOption>
-                    <MDBSelectOption value="MySQL">MySQL</MDBSelectOption>
-                    <MDBSelectOption value="NodeJS">NodeJS</MDBSelectOption>
-                    <MDBSelectOption value="Objective-C">Objective-C</MDBSelectOption>
-                    <MDBSelectOption value="OCaml">OCaml</MDBSelectOption>
-                    <MDBSelectOption value="Pascal">Pascal</MDBSelectOption>
-                    <MDBSelectOption value="Perl">Perl</MDBSelectOption>
-                    <MDBSelectOption value="PostgreSQL">PostgreSQL</MDBSelectOption>
-                    <MDBSelectOption value="PostScript">PostScript</MDBSelectOption>
-                    <MDBSelectOption value="PHP">PHP</MDBSelectOption>
-                    <MDBSelectOption value="Python">Python</MDBSelectOption>
-                    <MDBSelectOption value="React">React</MDBSelectOption>
-                    <MDBSelectOption value="Ruby">Ruby on Rails</MDBSelectOption>
-                    <MDBSelectOption value="Rust">Rust</MDBSelectOption>
-                    <MDBSelectOption value="Scala">Scala</MDBSelectOption>
-                    <MDBSelectOption value="SQL">SQL</MDBSelectOption>
-                    <MDBSelectOption value="Swift">Swift</MDBSelectOption>
-                    <MDBSelectOption value="Ubuntu">Ubuntu</MDBSelectOption>
-                    <MDBSelectOption value="Unix">Unix</MDBSelectOption>
-                    <MDBSelectOption value="Vue">Vue</MDBSelectOption>
-                    <MDBSelectOption value="WordPress">WordPress</MDBSelectOption>
-                  </MDBSelectOptions>
-                </MDBSelect>
-                {/* <label>Secondary Skills (optional):</label> */}
-              </div>
+              <MDBSelect
+                getValue={props.getValueOfSelectPrimary}
+              >
+                <MDBSelectInput selected="Choose one" />
+                <MDBSelectOptions search>
+                  <MDBSelectOption disabled>Choose one</MDBSelectOption>
+                  <MDBSelectOption value="Angular">Angular</MDBSelectOption>
+                  <MDBSelectOption value="ASP">ASP/ASP.NET</MDBSelectOption>
+                  <MDBSelectOption value="AWS">AWS</MDBSelectOption>
+                  <MDBSelectOption value="C#">C#</MDBSelectOption>
+                  <MDBSelectOption value="C++">C++</MDBSelectOption>
+                  <MDBSelectOption value="CSS">CSS</MDBSelectOption>
+                  <MDBSelectOption value="Debian">Debian</MDBSelectOption>
+                  <MDBSelectOption value="Drupal">Drupal</MDBSelectOption>
+                  <MDBSelectOption value="Elixir">Elixir/Erlang</MDBSelectOption>
+                  <MDBSelectOption value="Golang">Golang</MDBSelectOption>
+                  <MDBSelectOption value="GCP">Google GCP</MDBSelectOption>
+                  <MDBSelectOption value="Haskell">Haskell</MDBSelectOption>
+                  <MDBSelectOption value="Java">Java</MDBSelectOption>
+                  <MDBSelectOption value="JavaScript">JavaScript</MDBSelectOption>
+                  <MDBSelectOption value="Laravel">Laravel</MDBSelectOption>
+                  <MDBSelectOption value="Linux">Linux</MDBSelectOption>
+                  <MDBSelectOption value="Lisp">Lisp</MDBSelectOption>
+                  <MDBSelectOption value="MySQL">MySQL</MDBSelectOption>
+                  <MDBSelectOption value="NodeJS">NodeJS</MDBSelectOption>
+                  <MDBSelectOption value="Objective-C">Objective-C</MDBSelectOption>
+                  <MDBSelectOption value="OCaml">OCaml</MDBSelectOption>
+                  <MDBSelectOption value="Pascal">Pascal</MDBSelectOption>
+                  <MDBSelectOption value="Perl">Perl</MDBSelectOption>
+                  <MDBSelectOption value="PostgreSQL">PostgreSQL</MDBSelectOption>
+                  <MDBSelectOption value="PostScript">PostScript</MDBSelectOption>
+                  <MDBSelectOption value="PHP">PHP</MDBSelectOption>
+                  <MDBSelectOption value="Python">Python</MDBSelectOption>
+                  <MDBSelectOption value="React">React</MDBSelectOption>
+                  <MDBSelectOption value="Ruby">Ruby on Rails</MDBSelectOption>
+                  <MDBSelectOption value="Rust">Rust</MDBSelectOption>
+                  <MDBSelectOption value="Scala">Scala</MDBSelectOption>
+                  <MDBSelectOption value="SQL">SQL</MDBSelectOption>
+                  <MDBSelectOption value="Swift">Swift</MDBSelectOption>
+                  <MDBSelectOption value="Ubuntu">Ubuntu</MDBSelectOption>
+                  <MDBSelectOption value="Unix">Unix</MDBSelectOption>
+                  <MDBSelectOption value="Vue">Vue</MDBSelectOption>
+                  <MDBSelectOption value="WordPress">WordPress</MDBSelectOption>
+                </MDBSelectOptions>
+              </MDBSelect>
             </div>
-            <hr />
-            <div className="row upper">
-              <div className="col-md-6">
-                <h5>Position Level</h5>
-                <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-                  <button type="button" className="btn btn-blue-grey btn-sm">Junior</button>
-                  <button type="button" className="btn btn-blue-grey btn-sm">Senior</button>
-                  <button type="button" className="btn btn-blue-grey btn-sm">None</button>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <h5>Remote Position</h5>
-                <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-                  <button type="button" className="btn btn-blue-grey btn-sm">Yes</button>
-                  <button type="button" className="btn btn-blue-grey btn-sm">No</button>
-                  <button type="button" className="btn btn-blue-grey btn-sm">Either</button>
-                </div>
-              </div>
+            <div className="col-md-6">
+            <h5>Additional Skills</h5>
+              <MDBSelect
+                getValue={props.getValueOfSelectSecondary}
+                selected="Choose multiple"
+                multiple
+              >
+                <MDBSelectInput selected="Choose multiple" />
+                <MDBSelectOptions search>
+                  <MDBSelectOption disabled>Choose multiple</MDBSelectOption>
+                  <MDBSelectOption value="Angular">Angular</MDBSelectOption>
+                  <MDBSelectOption value="ASP">ASP/ASP.NET</MDBSelectOption>
+                  <MDBSelectOption value="AWS">AWS</MDBSelectOption>
+                  <MDBSelectOption value="C#">C#</MDBSelectOption>
+                  <MDBSelectOption value="C++">C++</MDBSelectOption>
+                  <MDBSelectOption value="CSS">CSS</MDBSelectOption>
+                  <MDBSelectOption value="Debian">Debian</MDBSelectOption>
+                  <MDBSelectOption value="Drupal">Drupal</MDBSelectOption>
+                  <MDBSelectOption value="Elixir">Elixir/Erlang</MDBSelectOption>
+                  <MDBSelectOption value="Golang">Golang</MDBSelectOption>
+                  <MDBSelectOption value="GCP">Google GCP</MDBSelectOption>
+                  <MDBSelectOption value="Haskell">Haskell</MDBSelectOption>
+                  <MDBSelectOption value="HTML">HTML</MDBSelectOption>
+                  <MDBSelectOption value="Java">Java</MDBSelectOption>
+                  <MDBSelectOption value="JavaScript">JavaScript</MDBSelectOption>
+                  <MDBSelectOption value="Laravel">Laravel</MDBSelectOption>
+                  <MDBSelectOption value="Linux">Linux</MDBSelectOption>
+                  <MDBSelectOption value="Lisp">Lisp</MDBSelectOption>
+                  <MDBSelectOption value="MySQL">MySQL</MDBSelectOption>
+                  <MDBSelectOption value="NodeJS">NodeJS</MDBSelectOption>
+                  <MDBSelectOption value="Objective-C">Objective-C</MDBSelectOption>
+                  <MDBSelectOption value="OCaml">OCaml</MDBSelectOption>
+                  <MDBSelectOption value="Pascal">Pascal</MDBSelectOption>
+                  <MDBSelectOption value="Perl">Perl</MDBSelectOption>
+                  <MDBSelectOption value="PostgreSQL">PostgreSQL</MDBSelectOption>
+                  <MDBSelectOption value="PostScript">PostScript</MDBSelectOption>
+                  <MDBSelectOption value="PHP">PHP</MDBSelectOption>
+                  <MDBSelectOption value="Python">Python</MDBSelectOption>
+                  <MDBSelectOption value="React">React</MDBSelectOption>
+                  <MDBSelectOption value="Ruby">Ruby on Rails</MDBSelectOption>
+                  <MDBSelectOption value="Rust">Rust</MDBSelectOption>
+                  <MDBSelectOption value="Scala">Scala</MDBSelectOption>
+                  <MDBSelectOption value="SQL">SQL</MDBSelectOption>
+                  <MDBSelectOption value="Swift">Swift</MDBSelectOption>
+                  <MDBSelectOption value="Ubuntu">Ubuntu</MDBSelectOption>
+                  <MDBSelectOption value="Unix">Unix</MDBSelectOption>
+                  <MDBSelectOption value="Vue">Vue</MDBSelectOption>
+                  <MDBSelectOption value="WordPress">WordPress</MDBSelectOption>
+                </MDBSelectOptions>
+              </MDBSelect>
             </div>
-            <hr />
-            <button type="button" className="btn-lg btn-primary">Go Get It!</button>
           </div>
-        </form>
-      </div>
-    );
-  }
+          <hr />
+          <div className="row upper">
+            <div className="col-md-6">
+              <h5>Position Level</h5>
+              <div className="btn-group" data-toggle="buttons">
+                <label className={props.positionValue==="Junior" ? "btn btn-blue-grey btn-sm form-check-label active" : "btn btn-blue-grey btn-sm form-check-label"}>
+                  <input className="form-check-input" type="radio" name={props.positionName} value="Junior" onClick={props.onChange} autoComplete="off" />
+                  Junior
+                </label>
+                <label className={props.positionValue==="Senior" ? "btn btn-blue-grey btn-sm form-check-label active" : "btn btn-blue-grey btn-sm form-check-label"}>
+                  <input className="form-check-input" type="radio" name={props.positionName} value="Senior" onClick={props.onChange} autoComplete="off" />
+                  Senior
+                </label>
+                <label className={props.positionValue==="None" ? "btn btn-blue-grey btn-sm form-check-label active" : "btn btn-blue-grey btn-sm form-check-label"}>
+                  <input className="form-check-input" type="radio" name={props.positionName} value="None" onClick={props.onChange} autoComplete="off" />
+                  None
+                </label>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <h5>Remote Position</h5>
+              <div className="btn-group" data-toggle="buttons">
+                <label className={props.remoteValue==="Yes" ? "btn btn-blue-grey btn-sm form-check-label active" : "btn btn-blue-grey btn-sm form-check-label"}>
+                  <input className="form-check-input" type="radio" name={props.remoteName} value="Yes" onClick={props.onChange} autoComplete="off" />
+                  Yes
+                </label>
+                <label className={props.remoteValue==="No" ? "btn btn-blue-grey btn-sm form-check-label active" : "btn btn-blue-grey btn-sm form-check-label"}>
+                  <input className="form-check-input" type="radio" name={props.remoteName} value="No" onClick={props.onChange} autoComplete="off" />
+                  No
+                </label>
+                <label className={props.remoteValue==="Either" ? "btn btn-blue-grey btn-sm form-check-label active" : "btn btn-blue-grey btn-sm form-check-label"}>
+                  <input className="form-check-input" type="radio" name={props.remoteName} value="Either" onClick={props.onChange} autoComplete="off" />
+                  Either
+                </label>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <button type="submit" className="btn-lg btn-primary" onClick={props.submit}>Go Get It!</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-
-
-  export default Form;
+export default Form;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,86 @@
 import React, { Component } from "react";
+import API from "../utils/API";
 import Nav from "../components/Nav"
 import Jumbotron from "../components/Jumbotron";
 import Form from "../components/Form";
+import {CardList, Card } from "../components/Card";
 import Footer from "../components/Footer";
 
 class Home extends Component {
   state = {
-    
+    jobs: [],
+    location: "",
+    position: "None",
+    remote: "Either",
+    primarySkills: [],
+    secondarySkills: []
   }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+
+    const query = "software developer"
+    API.searchJobs(query, this.state.location, 0)
+      .then(res => this.setState({ jobs: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  saveJob = jobData => {
+    API.saveJob(jobData)
+      .catch(err => console.log(err));
+  }
+
+  getValueOfSelectPrimary = value => {
+    this.setState({ primarySkills: value });
+  };
+
+  getValueOfSelectSecondary = value => {
+    this.setState({ secondarySkills: value });
+  };
 
   render() {
     return (
       <div>
-        <Nav />
+        <Nav
+          location={this.props.location.pathname}
+        />
         <Jumbotron />
-        <Form />
+        <Form
+          locationName="location"
+          locationValue={this.state.location}
+          getValueOfSelectPrimary={this.getValueOfSelectPrimary}
+          getValueOfSelectSecondary={this.getValueOfSelectSecondary}
+          positionName="position"
+          positionValue={this.state.position}
+          remoteName="remote"
+          remoteValue={this.state.remote}
+          onChange={this.handleInputChange}
+          submit={this.handleFormSubmit}
+        />
+        <CardList>
+          {this.state.jobs.map(job => {
+            const jobData = {
+              title: job.jobtitle,
+              company: job.company,
+              location: job.formattedLocation,
+              description: job.snippet,
+              url: job.url
+            }
+            return (
+              <Card
+                key={job.jobkey}
+                jobData={jobData}
+                saveOnClick={() => this.saveJob(jobData)}
+              />
+            );
+          })}
+        </CardList>
         <Footer />
       </div>
     );
