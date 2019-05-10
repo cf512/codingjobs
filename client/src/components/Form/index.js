16d077db145e85f01cce0d76f14597a0507ba37f
Connor F <connorfinnigan@gmail.com>
2019-05-10 02:14:43 -0500

    rearranged form and styling updates

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -56,26 +56,12 @@ class Form extends Component {
             <input type="text" className="form-control-lg" id="search-box" aria-describedby="emailHelp" placeholder="Enter City to Search" />
           </div>
           <div className="card card-body">
+
+            
+
             <div className="row upper">
               <div className="col-md-6">
-                <h5>Position</h5>
-                <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-                  <button type="button" className="btn btn-blue-grey btn-sm">None</button>
-                  <button type="button" className="btn btn-blue-grey btn-sm">Junior</button>
-                  <button type="button" className="btn btn-blue-grey btn-sm">Senior</button>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <h5>Remote Work</h5>
-                <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-                  <button type="button" className="btn btn-blue-grey btn-sm">Yes</button>
-                  <button type="button" className="btn btn-blue-grey btn-sm">No</button>
-                  <button type="button" className="btn btn-blue-grey btn-sm">Either</button>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
+              <h5>Primary Skill</h5>
                 <MDBSelect
                   getValue={this.getValueOfSelect}
                   getTextContent={this.getValueOfSelect}
@@ -125,9 +111,10 @@ class Form extends Component {
                     <MDBSelectOption value="WordPress">WordPress</MDBSelectOption>
                   </MDBSelectOptions>
                 </MDBSelect>
-                <label>Primary Skill (optional):</label>
+              {/* <label>Primary Skill:</label> */}
               </div>
               <div className="col-md-6">
+              <h5>Additional Skills</h5>
                 <MDBSelect
                   getValue={this.getValueOfSelect}
                   getTextContent={this.getValueOfSelect}
@@ -177,33 +164,29 @@ class Form extends Component {
                     <MDBSelectOption value="WordPress">WordPress</MDBSelectOption>
                   </MDBSelectOptions>
                 </MDBSelect>
-                <label>Secondary Skills (optional):</label>
+                {/* <label>Secondary Skills (optional):</label> */}
               </div>
             </div>
-            {/* <div className="row lower">
+            <hr />
+            <div className="row upper">
               <div className="col-md-6">
-                <select className="mdb-select md-form" multiple>
-                    <option value="" disabled selected>Choose Primary Skills</option>
-                    <option value="1">Java</option>
-                    <option value="2">Javascript</option>
-                    <option value="3">PHP</option>
-                    <option value="4">Python</option>
-                    <option value="5">React</option>
-                  </select>
-                  <button class="btn-save btn btn-primary btn-sm">Save</button>
+                <h5>Position Level</h5>
+                <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+                  <button type="button" className="btn btn-blue-grey btn-sm">Junior</button>
+                  <button type="button" className="btn btn-blue-grey btn-sm">Senior</button>
+                  <button type="button" className="btn btn-blue-grey btn-sm">None</button>
+                </div>
               </div>
               <div className="col-md-6">
-                <select className="mdb-select md-form" multiple>
-                    <option value="" disabled selected>Choose Secondary Skills</option>
-                    <option value="1">MySql</option>
-                    <option value="2">MongoDB</option>
-                    <option value="3">jQuery</option>
-                    <option value="4">Git</option>
-                    <option value="5">Japan</option>
-                  </select>
-                  <button className="btn-save btn btn-primary btn-sm">Save</button>
+                <h5>Remote Position</h5>
+                <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+                  <button type="button" className="btn btn-blue-grey btn-sm">Yes</button>
+                  <button type="button" className="btn btn-blue-grey btn-sm">No</button>
+                  <button type="button" className="btn btn-blue-grey btn-sm">Either</button>
+                </div>
               </div>
-            </div> */}
+            </div>
+            <hr />
             <button type="button" className="btn-lg btn-primary">Go Get It!</button>
           </div>
         </form>
