65094e00dcf50fd8fce6f7fd6721c62fc8f2c2c3
Connor F <connorfinnigan@gmail.com>
2019-05-14 01:06:53 -0500

    update to user db schema

--- models/user.js
+++ models/user.js
@@ -1,20 +1,134 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+/* eslint-disable no-console */
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  savedJobs: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Job"
-    }
-  ]
-});
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
-const User = mongoose.model("User", userSchema);
+module.exports = function () {
 
-module.exports = User;
+  var db = mongoose.connect("mongodb://localhost:27017/codingjobs", { useNewUrlParser: true });
+
+  var UserSchema = new Schema({
+    email: {
+      type: String, required: true,
+      trim: true, unique: true,
+      match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    },
+    facebookProvider: {
+      type: {
+        id: String,
+        token: String
+      },
+      select: false
+    },
+    twitterProvider: {
+      type: {
+        id: String,
+        token: String
+      },
+      select: false
+    },
+    googleProvider: {
+      type: {
+        id: String,
+        token: String
+      },
+      select: false
+    },
+    savedJobs: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Job"
+      }
+    ]
+  });
+
+  UserSchema.set("toJSON", {getters: true, virtuals: true});
+
+  // UserSchema.statics.upsertTwitterUser = function(token, tokenSecret, profile, cb) {
+  //   var that = this;
+  //   return this.findOne({
+  //     "twitterProvider.id": profile.id
+  //   }, function(err, user) {
+  //     // no user was found, lets create a new one
+  //     if (!user) {
+  //       var newUser = new that({
+  //         email: profile.emails[0].value,
+  //         twitterProvider: {
+  //           id: profile.id,
+  //           token: token,
+  //           tokenSecret: tokenSecret
+  //         }
+  //       });
+
+  //       newUser.save(function(error, savedUser) {
+  //         if (error) {
+  //           console.log(error);
+  //         }
+  //         return cb(error, savedUser);
+  //       });
+  //     } else {
+  //       return cb(err, user);
+  //     }
+  //   });
+  // };
+
+  UserSchema.statics.upsertFbUser = function(accessToken, refreshToken, profile, cb) {
+    var that = this;
+    return this.findOne({
+      "facebookProvider.id": profile.id
+    }, function(err, user) {
+      // no user was found, lets create a new one
+      if (!user) {
+        var newUser = new that({
+          fullName: profile.displayName,
+          email: profile.emails[0].value,
+          facebookProvider: {
+            id: profile.id,
+            token: accessToken
+          }
+        });
+
+        newUser.save(function(error, savedUser) {
+          if (error) {
+            console.log(error);
+          }
+          return cb(error, savedUser);
+        });
+      } else {
+        return cb(err, user);
+      }
+    });
+  };
+
+  UserSchema.statics.upsertGoogleUser = function(accessToken, refreshToken, profile, cb) {
+    var that = this;
+    return this.findOne({
+      "googleProvider.id": profile.id
+    }, function(err, user) {
+      // no user was found, lets create a new one
+      if (!user) {
+        var newUser = new that({
+          fullName: profile.displayName,
+          email: profile.emails[0].value,
+          googleProvider: {
+            id: profile.id,
+            token: accessToken
+          }
+        });
+
+        newUser.save(function(error, savedUser) {
+          if (error) {
+            console.log(error);
+          }
+          return cb(error, savedUser);
+        });
+      } else {
+        return cb(err, user);
+      }
+    });
+  };
+
+  mongoose.model("User", UserSchema);
+
+  return db;
+};
